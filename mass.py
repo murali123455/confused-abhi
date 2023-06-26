e8cc582e09a5da30dcb394432075cdb348a2c5ae
murali123455 <137748569+murali123455@users.noreply.github.com>
2023-06-26 10:16:42 +0530

    Update mass.py

--- mass.py
+++ mass.py
@@ -1,1 +1,11 @@
+hooks − This folder contains script files. Git hooks are the scripts that are executed before or after events like commit, push etc.
 
+objects − This folder represents an object database of Git.
+
+config − This is the local configuration file.
+
+refs − This folder stores information about tags and branches.
+
+HEAD − This file stores reference to the current branch. It points to the master branch by default.
+
+index − This is a binary file and stores staging information
